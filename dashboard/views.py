37e07c74fb38aa6bc5cab917cc6142ce25203aca
Atul Varma <varmaa@gmail.com>
2017-06-16 09:14:20 -0400

    Report percentage triaged within 1 business day.

--- dashboard/views.py
+++ dashboard/views.py
@@ -15,10 +15,13 @@ def get_stats():
     count = reports.count()
     accurates = reports.filter(is_accurate=True).count()
     false_negatives = reports.filter(is_false_negative=True).count()
+    triaged = reports.filter(days_until_triage__gte=0).count()
+    triaged_within_one_day = reports.filter(days_until_triage__lte=1).count()
 
     return {
         'triage_accuracy': percentage(accurates, count),
         'false_negatives': percentage(false_negatives, count),
+        'triaged_within_one_day': percentage(triaged_within_one_day, triaged)
     }
 
 
