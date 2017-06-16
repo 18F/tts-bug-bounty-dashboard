138b2d97395e2af61318c991476888d82d601e64
Atul Varma <varmaa@gmail.com>
2017-06-16 05:02:09 -0400

    show triage accuracy, false negatives.

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,24 @@ from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 
+from .models import Report
+
+
+def percentage(n, d):
+    return int((float(n) / float(d)) * 100.0)
+
+
+def get_stats():
+    reports = Report.objects.all()
+    count = reports.count()
+    accurates = reports.filter(is_accurate=True).count()
+    false_negatives = reports.filter(is_false_negative=True).count()
+
+    return {
+        'triage_accuracy': percentage(accurates, count),
+        'false_negatives': percentage(false_negatives, count),
+    }
+
 
 def index(request):
     scheme = 'http' if settings.DEBUG else 'https'
@@ -14,6 +32,8 @@ def index(request):
         },
         request=request
     ).replace('\n', '').replace('"', '&quot;'))
+
     return render(request, 'index.html', {
+        'stats': get_stats(),
         'bookmarklet_url': bookmarklet_url
     })
