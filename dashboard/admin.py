91e1c2e5ec066ad67a53d6fbe125f831fffc58a1
Jacob Kaplan-Moss <jacob@jacobian.org>
2017-08-31 14:04:54 -0700

    Added an admin filter for within/outside triage SLA
    This is helpful for us to review which issues are and aren't meeting the triage SLA quickly.

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-
 from .models import Report, SingletonMetadata
 
+class TriagedWithinSLAFilter(admin.SimpleListFilter):
+    title = "triage SLA"
+    parameter_name = 'met_triage_sla'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', "within triage SLA"),
+            ('no', "outside triage SLA")
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == "yes":
+            return queryset.filter(days_until_triage__lte=1)
+        elif self.value() == "no":
+            return queryset.filter(days_until_triage__gt=1)
 
 @admin.register(Report)
 class ReportAdmin(admin.ModelAdmin):
@@ -12,6 +26,8 @@ class ReportAdmin(admin.ModelAdmin):
         'state',
         'is_accurate',
         'is_false_negative',
+        'is_eligible_for_bounty',
+        'days_until_triage',
     )
 
     readonly_fields = Report.H1_OWNED_FIELDS + (
@@ -20,6 +36,14 @@ class ReportAdmin(admin.ModelAdmin):
         'next_nag_at',
     )
     fields = ('is_accurate', 'is_false_negative') + readonly_fields
+
+    list_filter = (
+        TriagedWithinSLAFilter,
+        'created_at',
+        'is_accurate',
+        'is_false_negative',
+        'is_eligible_for_bounty',
+    )
 
     def has_add_permission(self, request):
         return False
